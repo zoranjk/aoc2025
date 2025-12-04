f474d1bbc40deb14ae6cf0a8e993623e4d9d3514
zoranjk <kolegazoran@gmail.com>
2025-12-04 15:32:15 -0500

    day 4 part 2

--- day4.py
+++ day4.py
@@ -32,6 +32,52 @@ def p1(map) -> int:
 
     return accessible_spots
 
+
+def p2(map) -> int:
+    max_rolls = 3
+
+    adj_spots = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+    accessible_spots = 0
+
+    removed_roll = True
+
+    while removed_roll:
+        removed_roll = False
+
+        new_map = map.copy()
+
+        for col in map:
+            for row in map[col]:
+                ch = map[col][row]
+                if ch == '.':
+                    continue
+                
+                adj_rolls = 0
+                accessible = True
+                for dx, dy in adj_spots:
+                    adj_x = col + dx
+                    adj_y = row + dy
+
+                    if adj_x in map and adj_y in map[adj_x]:
+                        adj_ch = map[adj_x][adj_y]
+                        if adj_ch == '@':
+                            adj_rolls += 1
+                    
+                    if adj_rolls > max_rolls:
+                        accessible = False
+                        break
+
+                if accessible:
+                    accessible_spots += 1
+                    new_map[col][row] = '.'
+                    removed_roll = True
+
+        map = new_map
+
+    return accessible_spots
+
+
 if __name__ == "__main__":
     start = time()
     with open("day4.txt", "r") as f:
@@ -48,6 +94,7 @@ if __name__ == "__main__":
             map[i][j] = ch
             
     print(p1(map))
+    print(p2(map))
 
     end = time()
     print(f"Execution time: {end - start} seconds")
